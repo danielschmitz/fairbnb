d7f9e0114de3a1ed6be39b4edb674c94b6c3bf9c
Daniel Schmitz <danieljfa@gmail.com>
2023-03-07 10:35:19 -0300

    new migrations

--- prisma/migrations/20230307131639_create_user_table/migration.sql
+++ prisma/migrations/20230307131639_create_user_table/migration.sql
@@ -6,8 +6,12 @@ CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "role" "Role" NOT NULL DEFAULT 'USER',
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
