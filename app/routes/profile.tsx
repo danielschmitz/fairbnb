27333411e182b0c56dc0354d17edbaa3c999e808
Daniel Schmitz <danieljfa@gmail.com>
2023-03-20 11:32:43 -0300

    Change Password

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -10,6 +10,8 @@ import {
 } from "@remix-run/react";
 import { getUser, verify } from "~/login";
 import { commitSession, getSession } from "~/sessions";
+import bcrypt from "bcryptjs";
+
 
 export async function loader({ request }: LoaderArgs) {
   await verify(request, [Role.USER, Role.HOST, Role.ADMIN]);
@@ -18,9 +20,23 @@ export async function loader({ request }: LoaderArgs) {
 }
 
 export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const action = formData.get("action");
+
+  if (action === 'update') {
+    return await update(request, formData);
+  }
+
+  if (action === 'changePassword') {
+    return await changePassword(request, formData);
+  }
+
+  return "No action selected";
+}
+
+async function update(request: Request, formData: FormData) {
   const user: User = await getUser(request);
 
-  const formData = await request.formData();
   const email = formData.get("email") as User["email"];
   const name = formData.get("name") as User["name"];
   const id = Number(await formData.get("id"));
@@ -55,6 +71,46 @@ export async function action({ request }: ActionArgs) {
   });
 }
 
+async function changePassword(request: Request, formData: FormData) {
+  
+  const id = Number(await formData.get("id"));
+  const password = formData.get("password") as User["password"];
+  const newPassword = formData.get("newPassword") as User["password"];
+  const confirmPassword = formData.get("confirmPassword") as User["password"];
+
+  if (newPassword !== confirmPassword) {
+    return "Confirmation Password do not match";
+  }
+
+  const userData = await db.user.findFirst({
+    where: { id }, select: { password: true },
+  });
+
+  if (!userData) {
+    throw new Error("User not found");
+  }
+
+  if (!bcrypt.compareSync(password, userData.password)) {
+    return "Password is incorrect";
+  }
+
+  const hash = bcrypt.hashSync(newPassword, 10);
+
+  await db.user.update({
+    where: { id },
+    data: { password: hash },
+  }) 
+
+  const session = await getSession(request.headers.get("Cookie"));
+  session.flash("message", "Password updated");
+  return redirect("/", {
+    headers: {
+      "Set-Cookie": await commitSession(session),
+    },
+  });
+}
+
+
 export default function Profile() {
   const error = useActionData();
   const user = useLoaderData<typeof loader>();
@@ -64,13 +120,14 @@ export default function Profile() {
 
   return (
     <Form method="post">
+      {error && <div className="error">{error}</div>}
       <article>
         <header>
           <h2>Your Profile</h2>
         </header>
 
         <div style={{ maxWidth: 400, margin: "auto" }}>
-          {error && <div className="error">{error}</div>}
+          
 
           <input type="hidden" name="id" id="id" defaultValue={user.id} />
 
@@ -101,8 +158,61 @@ export default function Profile() {
 
         <footer className="form_footer">
           <Link to="/">Back</Link>
-          <button type="submit" disabled={busy}>
+          <button 
+            name="action"
+            value="update"
+            type="submit" 
+            disabled={busy}>
             {busy ? "Wait..." : "Update"}
+          </button>
+        </footer>
+      </article>
+      <article>
+        <header>
+          <h2>Change Password</h2>
+        </header>
+
+        <div style={{ maxWidth: 400, margin: "auto" }}>
+
+          <label htmlFor="name">
+            Password
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Your current password"
+            />
+          </label>
+
+          <label htmlFor="name">
+            New Password
+            <input
+              type="password"
+              id="newPassword"
+              name="newPassword"
+              placeholder="New Passowrd"
+            />
+          </label>
+
+          <label htmlFor="name">
+            Confirm New Password
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm new Passowrd"
+            />
+          </label>
+        </div>
+
+        <footer className="form_footer">
+          <Link to="/">Back</Link>
+          <button 
+            name="action"
+            value="changePassword"
+            type="submit" 
+            disabled={busy}>
+            {busy ? "Wait..." : "Change"}
           </button>
         </footer>
       </article>
