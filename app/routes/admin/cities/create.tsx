14c13d284bf466639120469c9063acb1206170cc
Daniel Schmitz <danieljfa@gmail.com>
2023-03-28 10:16:04 -0300

    create city

--- app/routes/admin/cities/create.tsx
+++ app/routes/admin/cities/create.tsx
@@ -1,17 +1,25 @@
 import { z } from "zod";
 import { makeDomainFunction } from "domain-functions";
-import { type ActionFunction } from "@remix-run/node";
+import { type LoaderArgs, type ActionFunction } from "@remix-run/node";
 import { formAction } from "~/form-action.server";
 import { Form } from "~/form";
 import CityService from "~/services/city.server";
+import CountryService from "~/services/country.server";
+import { useLoaderData } from "@remix-run/react";
 
 const schema = z.object({
   name: z.string().min(3).max(50),
+  countryId: z.number(),
 });
 
-const mutation = makeDomainFunction(schema)(async (values) =>
-  await CityService.create(values)
+const mutation = makeDomainFunction(schema)(
+  async (values) => await CityService.create(values)
 );
+
+export async function loader({ request }: LoaderArgs) {
+  const countries = await CountryService.all();
+  return countries;
+}
 
 export const action: ActionFunction = async ({ request }) =>
   formAction({
@@ -22,10 +30,32 @@ export const action: ActionFunction = async ({ request }) =>
   });
 
 export default function CreateCity() {
+  const countries = useLoaderData<typeof loader>();
+  const options = countries.map((country) => ({
+    name: country.name,
+    value: country.id,
+  }));
   return (
     <>
       <h5>Create City</h5>
-      <Form schema={schema} />
+      <Form schema={schema}>
+        {({ Field, Errors, Button }) => (
+          <>
+            <div className="grid">
+              <div>
+                <Field name="name"/>
+              </div>
+              <div>
+                <Field name="countryId" options={options} label="Country" />
+              </div>
+            </div>
+            <Errors />
+            <div style={{ display: "flex", justifyContent: "flex-end" }}>
+              <Button style={{ maxWidth: "100px" }} />
+            </div>
+          </>
+        )}
+      </Form>
     </>
   );
 }
